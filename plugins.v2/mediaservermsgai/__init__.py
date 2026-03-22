383dfe576f747f7ff124d2cf21ca982948f03208
dragon-tang <17402146+dragon-tang@users.noreply.github.com>
2026-03-22 09:17:02 +0800

    Update plugin version to 1.9.3

--- plugins.v2/mediaservermsgai/__init__.py
+++ plugins.v2/mediaservermsgai/__init__.py
@@ -43,7 +43,7 @@ class mediaservermsgai(_PluginBase):
     plugin_name = "媒体库服务器通知AI版"
     plugin_desc = "基于Emby识别结果+TMDB元数据+微信清爽版(全消息类型+剧集聚合+未识别过滤)"
     plugin_icon = "mediaplay.png"
-    plugin_version = "1.9.5"
+    plugin_version = "1.9.3"
     plugin_author = "jxxghp"
     author_url = "https://github.com/jxxghp"
     plugin_config_prefix = "mediaservermsgai_"
